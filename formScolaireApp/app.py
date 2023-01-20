fa4266cf6826d53a4d210d42eedbe29f97b10978
hakemi <hakemiayoub612@gmail.com>
2023-01-20 15:27:23 +0100

    Dash board building

--- formScolaireApp/app.py
+++ formScolaireApp/app.py
@@ -124,12 +124,12 @@ app.layout = html.Div(
     ],
     className="main-container",
 )
-     
+
+
 @app.callback(Output("graph", "children"), [Input("radios", "value")])
 def data_choice(value):
     content = ""
     if value == 1:
-        
         # La proportion de lycées professionnels labelisés 
         nbre_lycee_professionnel = df_result_all['code_etablissement'].nunique()
         nbre_lycee_professionel_labelise = df_result_all['rne'].nunique()
@@ -182,9 +182,193 @@ def data_choice(value):
         fig4 = px.histogram(df_proportion_lycee_non_labelise_annee, x="annee_y", y="code_etablissement",
                         color="annee_y",
                             labels={"code_etablissement" : "nombre de lycee non labelisés"})
+        fig4.update_layout(
+            autosize=False,
+            width=500,
+            height=250,
+            margin=dict(l=0, r=0, b=0, t=35, pad=0), 
+            title="La variation du nombre des lycées non labelisés selon les années",    
+            paper_bgcolor="#FFF7E9", 
+        )
 
-        return [dcc.Graph(figure=fig2),dcc.Graph(figure=fig3),dcc.Graph(figure=fig4)]
+        # transform label to numeric
+        df_result["label"] = pd.to_numeric(df_result["label"])
+        #sort label by ascending order
+        df_result.sort_values(by="label",inplace=True)
+        # re transform label to str so it can be a discrete value for visualisations (discrete value)
+        df_result["label"] = df_result["label"].apply(str)
+        
+        #plotting a histogram
+        #fig5 = px.histogram(df_result[df_result["resultat_apres_label"] == True], x="label", y="taux_brut_de_reussite_total_secteurs", nbins=10, histfunc="avg", color="label")
+        #fig5.update_layout(
+        #    autosize=False,
+        #    width=500,
+        #    height=250,
+        #    margin=dict(l=0, r=0, b=0, t=35, pad=0), 
+        #    title="La moyenne des taux bruts de réussites des lycee après obtention de leurs labels (tous secteurs) selon les labels",    
+        #    paper_bgcolor="#FFF7E9", 
+        #)
 
+        #plotting a histogram 
+        fig6 = px.histogram(df_result, x="departement_y", y="taux_brut_de_reussite_total_secteurs", nbins=10,
+                            histfunc="avg", color="departement_y",
+                            facet_row="resultat_apres_label",
+                            labels={"taux_brut_de_reussite_total_secteurs" : "taux brut de réussite"})
+        fig6.update_layout(
+            autosize=False,
+            width=1000,
+            height=500,
+            margin=dict(l=0, r=0, b=0, t=35, pad=0), 
+            title="La moyenne des taux bruts de réussites (tous secteurs) selon les départements - avant et après obtention labels",
+            # change the size of the title
+            title_font_size=10,    
+            paper_bgcolor="#FFF7E9", 
+        )
+
+        fig7 = px.histogram(df_result, x="resultat_apres_label", y="taux_brut_de_reussite_total_secteurs", nbins=10,
+                       histfunc="avg", color="resultat_apres_label",
+                      labels={"taux_brut_de_reussite_total_secteurs" : "taux brut de réussite"})
+        fig7.update_layout(title_text="La moyenne des taux bruts de réussites (tous secteurs) avant et après obtention labels")
+        
+        fig7.update_layout(
+            autosize=False,
+            width=500,
+            height=250,
+            margin=dict(l=0, r=0, b=0, t=35, pad=0), 
+            title="La moyenne des taux bruts de réussites (tous secteurs) avant et après obtention labels",
+            # change the size of the title
+            title_font_size=10,    
+            paper_bgcolor="#FFF7E9", 
+        )
+
+        fig8 = px.box(df_result_all, x="resultat_apres_label", y="taux_brut_de_reussite_total_secteurs",
+            color="resultat_apres_label",
+            labels={"taux_brut_de_reussite_total_secteurs" : "taux brut de réussite"})
+        fig8.update_layout(
+            autosize=False,
+            width=500,
+            height=250,
+            margin=dict(l=0, r=0, b=0, t=35, pad=0), 
+            title="La variance des taux bruts de réussites sans et avec labels",
+            # change the size of the title
+            title_font_size=10,    
+            paper_bgcolor="#FFF7E9", 
+        )
+
+        
+        fig9 = px.histogram(df_result, x="resultat_apres_label", y="va_reu_total", nbins=10, histfunc="avg", color="resultat_apres_label")
+        fig9.update_layout(
+            autosize=False,
+            width=500,
+            height=250,
+            margin=dict(l=0, r=0, b=0, t=35, pad=0), 
+            title="La moyenne des valeurs ajoutés de réussites (tous secteurs) avant et après obtention labels",
+            # change the size of the title
+            title_font_size=10,    
+            paper_bgcolor="#FFF7E9", 
+        )
+
+        fig10 = px.histogram(df_result_all, x="resultat_apres_label", y="va_reu_total", nbins=10, histfunc="avg", color="resultat_apres_label")
+        fig10.update_layout(
+            autosize=False,
+            width=500,
+            height=250,
+            margin=dict(l=0, r=0, b=0, t=35, pad=0), 
+            title="La moyenne des valeurs ajoutés de réussites (tous secteurs) sans et avec labels",
+            # change the size of the title
+            title_font_size=10,    
+            paper_bgcolor="#FFF7E9", 
+        )
+        
+        fig11 = px.histogram(df_result, x="resultat_apres_label", y="taux_reussite_attendu_france_total_secteurs", nbins=10,
+                       histfunc="avg", color="resultat_apres_label",
+                      labels={"taux_reussite_attendu_france_total_secteurs" : "Taux_réussite_attendu"})
+        fig11.update_layout(
+            autosize=False,
+            width=500,
+            height=250,
+            margin=dict(l=0, r=0, b=0, t=35, pad=0), 
+            title="La moyenne des taux de réussite attendu en france (tous secteurs) avant et après obtention labels",
+            # change the size of the title
+            title_font_size=10,    
+            paper_bgcolor="#FFF7E9", 
+        )
+
+        fig12 = px.histogram(df_result_all, x="resultat_apres_label", y="taux_reussite_attendu_france_total_secteurs", nbins=10,
+                       histfunc="avg", color="resultat_apres_label",
+                      labels={"taux_reussite_attendu_france_total_secteurs" : "Taux_réussite_attendu"})
+        fig12.update_layout(
+            autosize=False,
+            width=500,
+            height=250,
+            margin=dict(l=0, r=0, b=0, t=35, pad=0), 
+            title="La moyenne des taux de réussite attendu en france (tous secteurs) sans et avec labels",
+            # change the size of the title
+            title_font_size=10,    
+            paper_bgcolor="#FFF7E9", 
+        )
+
+        content = html.Div([
+            #html.H1("Analyse des données"),
+            #html.Hr(),
+            html.Div([
+                html.Div([
+                    dcc.Graph(figure=fig2),
+                ], className="columns"),
+            ],
+             className="row1"),
+
+            html.Div([
+                html.Div([
+                    dcc.Graph(figure=fig3),
+                ], className="columns"),
+                html.Div([
+                    dcc.Graph(figure=fig4),
+                ], className="columns"),
+            ],
+             className="row2"),
+            
+            html.Div([
+                #html.Div([
+                #    dcc.Graph(figure=fig5),
+                #], className="columns"),
+                html.Div([
+                    dcc.Graph(figure=fig6),
+                ], className="columns"),
+            ],
+             className="row3"),
+            
+            
+            html.Div([
+                html.Div([
+                    dcc.Graph(figure=fig7),
+                ], className="columns"),
+                html.Div([
+                    dcc.Graph(figure=fig8),
+                ], className="columns"),
+            ],
+             className="row4"),
+
+            html.Div([
+                html.Div([
+                    dcc.Graph(figure=fig9),
+                ], className="columns"),
+                html.Div([
+                    dcc.Graph(figure=fig10),
+                ], className="columns"),
+            ], className="row5"),
+
+            html.Div([
+                html.Div([
+                    dcc.Graph(figure=fig11),
+                ], className="columns"),
+                html.Div([
+                    dcc.Graph(figure=fig12),
+                ], className="columns"),
+            ], className="row6"),
+        ], className="container")
+
+        return content
 
 
 # run the app
